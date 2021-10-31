03285cf7740d5904033b172d2fb2377c1ecc7f35
Ezgi Yaman <ezgimm_97@hotmail.com>
2021-10-31 15:44:09 +0300

    added

---
+++ DRY_Principle/BadExample/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRY_Principle.BadExample
+{
+    public class Cart
+    {
+        public int GetUserCredit()
+        {
+            var userCredit = 25;
+
+            return userCredit + 1000;
+        }
+    }
+}
---
+++ DRY_Principle/GoodExample/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRY_Principle.GoodExample
+{
+    public class Cart
+    {
+        public double GetUserCredit()
+        {
+            var userCredit = 25;
+
+            return UserHelper.GetUserCredit(userCredit);
+        }
+    }
+}
